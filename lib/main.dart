43b1c5126344f8992bddcb58ce130d021dae0898
MohamedAlkindi <msbakng@gmail.com>
2025-03-26 23:12:51 +0000

    feat: reintroduce Firebase options and initialize Firebase in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:finals_project/core/database/database.dart';
+import 'package:finals_project/core/firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await AppDatabase.initializeDB();
+
   runApp(const MainApp());
 }
 
