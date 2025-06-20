8fd614fca6bf366ce600999753bbe20463d9b949
MohamedAlkindi <msbakng@gmail.com>
2025-06-21 00:44:05 +0300

    supabase storage setup

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ import 'package:event_connect/features/welcome_screen/presentation/welcome_scree
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 FirebaseUser user = FirebaseUser();
 FirebaseLogin loginCheckup = FirebaseLogin();
@@ -60,6 +61,10 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await Supabase.initialize(
+      url: 'https://mretvbuvvmcbjdivlogc.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZXR2YnV2dm1jYmpkaXZsb2djIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NTM0MzgsImV4cCI6MjA2NjAyOTQzOH0.xPz552lN_TfgO1RX7CjHYheRJ04nmio0WV_Z6Yw-7Fw');
   startUpWidget = await whichWidget();
   runApp(const MainApp());
 }
